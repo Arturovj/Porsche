cbf1d56cf0630edd3a9b6bbec0fc940c651fbd5d
Arturo Vivar <arthas1237@hotmail.com>
2022-07-03 19:33:45 +0200

    911

--- pages/porsche911.js
+++ pages/porsche911.js
@@ -1,11 +1,67 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import { motion } from 'framer-motion'
+import Image from 'next/image'
+
+const imageAnimate = {
+  offscreen: { x:-100, opacity: 0 },
+  onscreen: { x: 0, opacity: 1 },
+  transition: {
+    type: 'spring',
+    duration: 5,
+  }
+}
+
+const textAnimate = {
+  offscreen: { y: 100, opacity: 0 },
+  onscreen: { y: 0, opacity: 1 },
+  transition: {
+    type: 'spring',
+    duration: 5,
+  }
+}
+
+
+
 
 export default function porsche911() {
   return (
     <Layout>
-
-<div>911</div>    
+      <motion.div
+      initial={'offscreen'}
+      whileInView={'onscreen'}
+      viewport={{ once: false, amount: 0.5}}
+      transition={{ staggerChildren: 5}}
+      variants={imageAnimate}
+      >
+       <Image src="/img/911page1.jpg" alt="Porsche 911" width={1280} height={720} />
+      </motion.div>
+      
+      <div>
+      <motion.p
+      initial={'offscreen'}
+      whileInView={'onscreen'}
+      variants={textAnimate}
+      >
+      <div><h1>911</h1></div>  
+      </motion.p>
+      <motion.div
+      initial={'offscreen'}
+      whileInView={'onscreen'}
+      viewport={{ once: false, amount: 0.5}}
+      transition={{ staggerChildren: 5}}
+      variants={imageAnimate}
+      >
+       <Image src="/img/911page2.jpg" alt="Porsche 911" width={720} height={480} />
+      </motion.div>
+      </div>
+      <motion.p
+      initial={'offscreen'}
+      whileInView={'onscreen'}
+      variants={textAnimate}
+      >
+      <div>911</div>  
+      </motion.p>
     </Layout>
     
   )
